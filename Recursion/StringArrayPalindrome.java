d0dcc7f522ecf9011a2d5750a3b9099adf4107d5
reddirani8496 <reddiranibattuluri@gmail.com>
2025-07-28 23:31:33 +0530

    valid palindrome program uploaded

---
+++ Recursion/StringArrayPalindrome.java
@@ -0,0 +1,32 @@
+package Recursion;
+
+
+public class StringArrayPalindrome {
+     public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+    }
+
+
+
+    public static boolean isPalindrome(String s) {
+        // A man, a plan, a canal: Panama
+        int left = 0;
+        int right = s.length() - 1;
+        while (left <= right) {
+            
+            if (!Character.isLetterOrDigit(s.charAt(left)))
+                left++;
+            else if (!Character.isLetterOrDigit(s.charAt(right)))
+                right--;
+
+            else if (!s.substring(left,left+1).equalsIgnoreCase(s.substring(right,right+1)))
+                return false;
+            else if (s.substring(left,left+1).equalsIgnoreCase(s.substring(right,right+1))) {
+                left++;
+                right--;
+            }
+
+        }
+        return true;
+    }
+}
