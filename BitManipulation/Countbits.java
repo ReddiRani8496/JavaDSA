6ec40a90f390648b29ea814da196a5465ee66bfe
reddirani8496 <reddiranibattuluri@gmail.com>
2025-08-20 20:13:00 +0530

    Count set bits program uploaded

---
+++ BitManipulation/Countbits.java
@@ -0,0 +1,19 @@
+package BitManipulation;
+
+import java.util.Scanner;
+
+public class Countbits {
+       public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int count = 0;
+      // 1010
+        while (n!=0) {
+           if( (n & 1) !=0)
+               count++;
+            n = n>>1;
+        }
+
+        System.out.println(count);
+    }
+}
