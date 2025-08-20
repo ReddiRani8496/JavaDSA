7fd12f2dadd41ddde76555905ca4e7765e95c928
reddirani8496 <reddiranibattuluri@gmail.com>
2025-08-20 20:05:21 +0530

    Swap two numbers program uploaded

--- BitManipulation/CheckKthBitSet.java
+++ BitManipulation/CheckKthBitSet.java
@@ -3,7 +3,7 @@ package BitManipulation;
 public class CheckKthBitSet {
     public static void main(String[] args) {
         int n = 10; // 1010
-        int k = 2;
+        int k = 1;
         int mask = 1;
         if((n &(mask<<k))!=0) {
             System.out.println("set bit");
